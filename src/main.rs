19f98cab20f5ce6dfe1d574f88c192c19dabd148
jenspetur94 <jenspeturtrondarson@gmail.com>
2019-02-16 13:41:52 +0100

    Initial-commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+use crate::chip8::Chip8;
+
+mod ram;
+mod chip8;
+fn main() {
+    let mut file = File::open("Data/INVADERS").unwrap();
+
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+
+    let mut chip8 = Chip8::new();
+
+    chip8.load_rom(&data);
+}
